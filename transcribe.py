11f160c535ef169dd13e4c3e45b365155324cb0f
Nishant Babel <babel.nishant@gmail.com>
2023-04-29 00:52:46 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -14,6 +14,7 @@ def get_url(token,data):
   response = requests.post('https://api.assemblyai.com/v2/upload',
                          headers=headers,
                          data=data)
+  print(response)
   url = response.json()["upload_url"]
   print("Uploaded File and got temporary URL to file")
   return url
